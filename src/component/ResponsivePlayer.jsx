d2660d83ba0818037472d1fa4016c35af83de1b9
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-11-12 17:46:24 +0100

    wip

--- src/component/ResponsivePlayer.jsx
+++ src/component/ResponsivePlayer.jsx
@@ -8,7 +8,7 @@ const ResponsivePlayer = ({ url, onProgress }) => {
   return (
     <div className='player-wrapper'>
       <ReactPlayer
-        className='react-player' width="100%" height="100%"
+        className='react-player' width="100%" height="100vh"
         url={url}
 
         controls={true}
